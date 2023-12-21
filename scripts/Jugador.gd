ed8e2bb438b31781de51eccda17ddbc059b55fbe
JorgeSaez1 <jorgesaez952o@gmail.com>
2023-12-20 23:50:46 -0300

    movimientos player

--- scripts/Jugador.gd
+++ scripts/Jugador.gd
@@ -1,12 +1,20 @@
 extends CharacterBody2D
 
-
 const Velocidad	 = 300.0
-const Movimiento = Vector2()
+var Movimiento = Vector2()
+var Touch = false
+var Posicion 
 
 func _input(event):
 	if event is InputEventScreenTouch:
-		
+		if event.pressed == true:
+			Touch = true
+			Posicion = event.position
+		else:
+			Touch = false
 func _ready():
+	pass
 func _physics_process(delta):
+	
+	pass
 	
