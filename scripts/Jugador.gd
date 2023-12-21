265da5aac0c79ed24f9db697b0b2edcf790c8055
JorgeSaez1 <jorgesaez952o@gmail.com>
2023-12-20 23:27:02 -0300

    primera scena y configuracion

---
+++ scripts/Jugador.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+
+const Velocidad	 = 300.0
+const Movimiento = Vector2()
+
+func _input(event):
+	if event is InputEventScreenTouch:
+		
+func _ready():
+func _physics_process(delta):
+	
