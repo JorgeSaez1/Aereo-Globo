21495c7e570fd127776e2bc4c1ac369dea7e6b06
Jorge <jorgesaez952o@gmail.com>
2024-01-04 14:44:14 -0300

    rotacion de enemigos

--- scripts/Escena_principal.gd
+++ scripts/Escena_principal.gd
@@ -15,7 +15,30 @@ func nuevo_juego():
 	Score = 0
 	$InicioTimer.start()
 	$Jugador.inicio($PosicionInicial.Posicion)
+	$AudioJugando.start()
 	
 func game_over():
 	$ScoreTimer.stop()
 	$BolaTimer.stop()
+	$AudioGame_Over.start()
+	$AudioJugando.stop()
+
+
+func _on_inicio_timer_timeout():
+	$BolaTimer.start()
+	$ScoreTimer.start()
+
+
+func _on_score_timer_timeout():
+	Score += 1
+
+func _on_bola_timer_timeout():
+	$BolaCamino/BolaPosicion.set_offset(randi())
+	var B = Bola.instance()
+	add_child(B)
+	var d = $BolaCamino/BolaPosicion.rotation + PI / 2
+	B.position = $BolaCamino/BolaPosicion.position
+	d += randf_range(-PI/4,PI/4)
+	B.rotation = d
+	
+	
