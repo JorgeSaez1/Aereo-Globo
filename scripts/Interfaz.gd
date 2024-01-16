d6e466276d55db6a99a138869225384a4a3a86d9
JorgeSaez1 <jorgesaez952o@gmail.com>
2024-01-16 01:06:55 -0300

    Script interfaz

--- scripts/Interfaz.gd
+++ scripts/Interfaz.gd
@@ -1,11 +1,28 @@
 extends CanvasLayer
-
+signal Nuevo_Juego
+var P = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Instrucciones.hide()
+	$BotonVolver.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func Mensaje(texto):
+	$Mensaje.text = texto
+	$Mensaje.show()
+	$MensajeTimer.start()
+
+
+func _on_mensaje_timer_timeout():
+	$Mensaje.hide()
+	
+func Score(Score):
+	$Score.text = str(Score)
+	$Score.show()
+	
+func Game_Over():
+	P = true
+	
+	
