fb8832094a49a14401957190ba9c5e79bd709abb
JorgeSaez1 <jorgesaez952o@gmail.com>
2024-03-14 15:54:38 -0300

    pregreso bola

--- scripts/Escena_principal.gd
+++ scripts/Escena_principal.gd
@@ -1,10 +1,21 @@
 extends Node
-var Bola = preload("res://scenes/Enemigo.tscn").instantiate()
+#var Bola = load("res://scenes/Enemigo.tscn").instantiate()
+var Bola = load("res://scenes/Enemigo.tscn")
+
+#var Bola = Scene.Enemigo()
 var Score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
+	$BolaCamino/BolaPosicion.set_progress(randi() )
+	var B = Bola.instantiate()
+	add_child(B)
+	var d = $BolaCamino/BolaPosicion.rotation + PI / 2
+	B.position = $BolaCamino/BolaPosicion.position
+	d += randf_range(-PI/4,PI/4)
+	B.rotation = d
+	B.set_linear_velocity(Vector2(randf_range(B.VelocidadMin,B.VelocidadMax), 0).rotated(d))
 	
 	
 
@@ -36,15 +47,15 @@ func _on_score_timer_timeout():
 	Score += 1
 	$Interfaz.Score(Score)
 
-func _on_bola_timer_timeout():
-	$BolaCamino/BolaPosicion.set_progress(randi() )
-	var B = Bola
+#func _on_bola_timer_timeout():
+#	$BolaCamino/BolaPosicion.set_progress(randi() )
+	#var B = Bola.instantiate()
 	#add_child(B)
-	var d = $BolaCamino/BolaPosicion.rotation + PI / 2
-	B.position = $BolaCamino/BolaPosicion.position
-	d += randf_range(-PI/4,PI/4)
-	B.rotation = d
-	B.set_linear_velocity(Vector2(randf_range(B.VelocidadMin,B.VelocidadMax), 0).rotated(d))
+#	var d = $BolaCamino/BolaPosicion.rotation + PI / 2
+#	B.position = $BolaCamino/BolaPosicion.position
+#	d += randf_range(-PI/4,PI/4)
+#	B.rotation = d
+#	B.set_linear_velocity(Vector2(randf_range(B.VelocidadMin,B.VelocidadMax), 0).rotated(d))
 	
 	
 	
